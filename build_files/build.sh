ff507b9a0909cb16ad766b614e3980a5f780b850
Sam Dasilva <85967506+SamD2021@users.noreply.github.com>
2026-01-17 03:54:43 +0000

    feat(setup): Add cosing pub key and switch base (#1)
    Signed-off-by: Samuel DaSilva <samuelramos852@gmail.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,9 +8,12 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
+dnf5 -y copr enable scottames/ghostty
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+	tmux \
+	just \
+	ghostty
 
 # Use a COPR Example:
 #

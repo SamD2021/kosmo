857be76086bd9658eed7dbd17e02b6724a0adc40
Sam DaSilva <sam.ramos.dasilva@gmail.com>
2026-04-21 01:09:30 -0400

    fix(1password): enforce op setgid/group ownership in image build
    Signed-off-by: Sam DaSilva <sam.ramos.dasilva@gmail.com>

--- build_files/build-packages.sh
+++ build_files/build-packages.sh
@@ -11,54 +11,66 @@ sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://down
 
 # Aggressive GNOME desktop purge: fail hard on conflicts.
 dnf5 remove -y \
-  gdm \
-  gnome-shell \
-  gnome-shell-common \
-  gnome-session \
-  gnome-session-wayland-session \
-  gnome-session-xsession \
-  mutter \
-  mutter-common \
-  gnome-shell-extension-apps-menu \
-  gnome-shell-extension-background-logo \
-  gnome-shell-extension-common \
-  gnome-shell-extension-launch-new-instance \
-  gnome-shell-extension-places-menu \
-  gnome-shell-extension-pop-shell \
-  gnome-shell-extension-pop-shell-shortcut-overrides \
-  gnome-shell-extension-window-list
+	gdm \
+	gnome-shell \
+	gnome-shell-common \
+	gnome-session \
+	gnome-session-wayland-session \
+	gnome-session-xsession \
+	mutter \
+	mutter-common \
+	gnome-shell-extension-apps-menu \
+	gnome-shell-extension-background-logo \
+	gnome-shell-extension-common \
+	gnome-shell-extension-launch-new-instance \
+	gnome-shell-extension-places-menu \
+	gnome-shell-extension-pop-shell \
+	gnome-shell-extension-pop-shell-shortcut-overrides \
+	gnome-shell-extension-window-list
 
 # Install COSMIC identity on top of Silverblue base
 dnf5 install --allowerasing -y \
-  fedora-asahi-remix-release-cosmic-atomic \
-  fedora-asahi-remix-release-identity-cosmic-atomic
+	fedora-asahi-remix-release-cosmic-atomic \
+	fedora-asahi-remix-release-identity-cosmic-atomic
 
 dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
 dnf5 install -y \
-  tmux \
-  just \
-  ghostty \
-  zen-browser \
-  asahi-nvram \
-  zsh \
-  gcc \
-  pam-u2f \
-  pamu2fcfg \
-  libfido2 \
-  qemu \
-  libvirt \
-  libvirt-devel \
-  virt-install \
-  genisoimage \
-  virt-manager \
-  openvswitch \
-  swtpm \
-  tailscale \
-  greetd \
-  lxpolkit \
-  1password-cli \
-  @cosmic-desktop-environment
+	tmux \
+	just \
+	ghostty \
+	zen-browser \
+	asahi-nvram \
+	zsh \
+	gcc \
+	pam-u2f \
+	pamu2fcfg \
+	libfido2 \
+	qemu \
+	libvirt \
+	libvirt-devel \
+	virt-install \
+	genisoimage \
+	virt-manager \
+	openvswitch \
+	swtpm \
+	tailscale \
+	greetd \
+	lxpolkit \
+	1password-cli \
+	@cosmic-desktop-environment
+
+# Enforce Linux app-integration permissions for immutable systems.
+if ! getent group onepassword-cli >/dev/null; then
+	groupadd onepassword-cli
+fi
+
+for op_bin in /usr/bin/op /usr/sbin/op; do
+	if [ -e "$op_bin" ]; then
+		chgrp onepassword-cli "$op_bin"
+		chmod 2755 "$op_bin"
+	fi
+done
 
 # Ensure Widevine prefs are always present in immutable /opt payload.
 install -d /opt/zen-browser/defaults/pref
